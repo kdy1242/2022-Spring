0cb4ef5a53520ea1cd23544e45631f18fb5eaf18
kdy1242 <dy12311229@gmail.com>
2022-04-26 12:19:55 +0900

    PointcutCommon

--- src/main/java/com/mirim/biz/common/AfterThrowingAdvice.java
+++ src/main/java/com/mirim/biz/common/AfterThrowingAdvice.java
@@ -5,6 +5,15 @@ import org.aspectj.lang.JoinPoint;
 public class AfterThrowingAdvice {
     public void exceptionLog(JoinPoint jp, Exception exceptionObj) {
         String method = jp.getSignature().getName();
-        System.out.println("[예외처리]" + method + "() 메소드 수행 중 발생된 예외 메세지 : " + exceptionObj.getMessage());
+
+        System.out.println("[예외처리]" + method + "() 메소드 수행 중 예외 발생");
+
+        if (exceptionObj instanceof IllegalArgumentException) {
+            System.out.println("부적합한 값이 입력되었습니다.");
+        } else if (exceptionObj instanceof NumberFormatException) {
+            System.out.println("숫자 형식의 값이 아닙니다.");
+        } else if (exceptionObj instanceof Exception) {
+            System.out.println("에러가 발생");
+        }
     }
 }
--- src/main/java/com/mirim/biz/common/AroundAdvice.java
+++ src/main/java/com/mirim/biz/common/AroundAdvice.java
@@ -1,12 +1,24 @@
 package com.mirim.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
 
+@Service
+@Aspect
 public class AroundAdvice {
+
+    @Around("PointcutCommon.allPointcut()")
     public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
-        System.out.println("[Before] 비즈니스 메소드 수행전에 처리할 내용");
-        Object returnObj = pjp.proceed();
-        System.out.println("[After] 비즈니스 메소드 수행후에 처리할 내용");
-        return returnObj;
+        String method = pjp.getSignature().getName();
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();  // before 처리
+        Object obj = pjp.proceed();  // Spring 컨테이너를 통해 사용자의 요청 처리
+        stopWatch.stop();   // after
+        System.out.println(method + "()메소드 수행에 걸린 시간 : " + stopWatch.getTotalTimeMillis() + "(ms)초");
+        return obj;  // 사용자에게 리턴
     }
 }
